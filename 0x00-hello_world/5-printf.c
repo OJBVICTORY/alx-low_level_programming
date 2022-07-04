f21952dc23331238a6e512194b8c646448380b77
Ojugba O. Victory <ojugbaovictory@gmail.com>
2022-07-03 20:06:23 -0700

    task 5 corrected

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 /**
- *Main - Entry point
- *
- *Return: Always 0 (Success)
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
